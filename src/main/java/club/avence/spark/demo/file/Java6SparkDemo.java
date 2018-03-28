d1b29fef84ffc289f0ce556a3e47e695b6cba75b
c_qiancheng <abcqdsdchina@126.com>
2018-03-28 13:35:25 +0800

    代码重构，摒弃Set的实现去重，采用Spark算子实现去重

--- src/main/java/club/avence/spark/demo/file/Java6SparkDemo.java
+++ src/main/java/club/avence/spark/demo/file/Java6SparkDemo.java
@@ -1,42 +1,46 @@
 package club.avence.spark.demo.file;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
-import org.apache.spark.api.java.function.Function2;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
 
 /**
  * Created by qian3 on 2018/3/4.
  */
 public class Java6SparkDemo {
+
+    private static final Logger log = LoggerFactory.getLogger(Java6SparkDemo.class);
+
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("SparkDemo").setMaster("local[1000]");
-        JavaSparkContext context = new JavaSparkContext(conf);
-        JavaRDD<String> lines = context.textFile(Java6SparkDemo.class.getResource(".").getFile() + File.separator + "SparkDemo.iml");
+        JavaSparkContext context = null;
+        try {
+            context = new JavaSparkContext(conf);
+            JavaRDD<String> lines = context.textFile(Java6SparkDemo.class.getResource("/").getFile() + File.separator + "SparkDemo.iml");
 
-        Set<String> updatedIndexCodes = lines.map(new Function<String, Set<String>>() {
-            @Override
-            public Set<String> call(String line) {
-                Set<String> indexCodes = new HashSet<>();
-                if (StringUtils.isNotBlank(line)) {
-                    String[] columns = line.split("\\|");
-                    indexCodes.add(columns[2] + "|" + columns[3]);
+            List<String> updatedIndexCodes = lines.map(new Function<String, String>() {
+                @Override
+                public String call(String line) {
+                    if (StringUtils.isNotBlank(line)) {
+                        String[] columns = line.split("\\|");
+                        return columns[2] + "|" + columns[3];
+                    }
+                    return null;
                 }
-                return indexCodes;
-            }
-        }).reduce(new Function2<Set<String>, Set<String>, Set<String>>() {
-            @Override
-            public Set<String> call(Set<String> a, Set<String> b) {
-                a.addAll(b);
-                return a;
-            }
-        });
-        System.out.println(updatedIndexCodes.size());
+            }).distinct().collect();
+            log.info("去重的指标代码数量：{}。", updatedIndexCodes.size());
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            IOUtils.closeQuietly(context);
+        }
     }
 }
